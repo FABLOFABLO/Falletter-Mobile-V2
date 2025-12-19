582edddd37a0f6bedf2f0e1bc8ef8f4ec50682f7
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-12-19 10:22:03 +0900

    chore: initialize main.dart structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,33 @@
+import 'package:falletter_mobile_v2/core/components/bottom_navigatoin_bar/custon_bottom_nav_bar.dart';
 import 'package:falletter_mobile_v2/core/constants/color.dart';
+import 'package:falletter_mobile_v2/core/router/app_router.dart';
 import 'package:falletter_mobile_v2/core/theme/falletter_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      ProviderScope(
+          child: const MyApp(),
+      ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(goRouterProvider);
+
+    return MaterialApp.router(
+      routerConfig: router,
       theme: ThemeData(
         scaffoldBackgroundColor: FalletterColor.black,
         inputDecorationTheme: inputDecorationTheme,
         textSelectionTheme: textSelectionTheme,
       ),
-      home: ,
     );
   }
 }
