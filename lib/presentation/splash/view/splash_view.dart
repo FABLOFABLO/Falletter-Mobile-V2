6b5ba46f1e99a5f5a55c29d1b841c96f4f15b329
정지윤 <163826760+jiyun040@users.noreply.github.com>
2025-12-30 21:16:02 +0900

    refactor: modify import path

--- lib/presentation/splash/view/splash_view.dart
+++ lib/presentation/splash/view/splash_view.dart
@@ -4,7 +4,7 @@ import 'package:falletter_mobile_v2/core/constants/color.dart';
 import 'package:falletter_mobile_v2/core/constants/text_style.dart';
 import 'package:falletter_mobile_v2/core/providers/theme/theme_state.dart';
 import 'package:falletter_mobile_v2/core/theme/app_theme_color.dart';
-import 'package:falletter_mobile_v2/presentation/signup/view/set_gender_view.dart';
+import 'package:falletter_mobile_v2/presentation/signup/view/gender_view.dart';
 import 'package:falletter_mobile_v2/presentation/splash/provider/auth_status_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
