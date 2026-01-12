9475c716828da92d406fd211b13b920563f2d48f
최승우 <choi0250655@gmail.com>
2026-01-12 23:33:04 +0900

    gender_view 제작
    gender_view 제작

--- lib/presentation/signup/view/gender_view.dart
+++ lib/presentation/signup/view/gender_view.dart
@@ -1,11 +1,82 @@
-import 'package:flutter/material.dart';
+import 'package:falletter_mobile_v2/core/components/app_bar/custom_app_bar.dart';
+import 'package:falletter_mobile_v2/core/components/button/elevated_button.dart';
+import 'package:falletter_mobile_v2/core/components/button/selectable_button.dart';
+import 'package:falletter_mobile_v2/core/constants/color.dart';
+import 'package:falletter_mobile_v2/core/constants/text_style.dart';
+import 'package:flutter/material.dart' hide Action;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
 
-class SetGenderView extends ConsumerWidget {
+enum Gender { man, woman }
+
+class SetGenderView extends ConsumerStatefulWidget {
   const SetGenderView({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const Placeholder();
+  ConsumerState<SetGenderView> createState() => _SetGenderViewState();
+}
+
+class _SetGenderViewState extends ConsumerState<SetGenderView> {
+  Gender? gender;
+  final Color blue = FalletterColor.blueGradient.first;
+  final Color pink = FalletterColor.pinkGradient.first;
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isEnabled = gender != null;
+    return Scaffold(
+      appBar: CustomAppBar(icon: true, action: Action.orderStep, count: 1),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 28),
+              Text('성별을 선택해주세요', style: FalletterTextStyle.title2),
+              const SizedBox(height: 36),
+              Row(
+                children: [
+                  Expanded(
+                    child: SelectableButton(
+                      label: '남성',
+                      icon: Symbols.man,
+                      iconColor: blue,
+                      isSelected: gender == Gender.man,
+                      onTap: () {
+                        setState(() => gender = Gender.man);
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 24),
+                  Expanded(
+                    child: SelectableButton(
+                      label: '여성',
+                      icon: Symbols.woman,
+                      iconColor: pink,
+                      isSelected: gender == Gender.woman,
+                      onTap: () {
+                        setState(() => gender = Gender.woman);
+                      },
+                    ),
+                  ),
+                ],
+              ),
+
+              const Spacer(),
+
+              CustomElevatedButton(
+                onPressed: isEnabled
+                    ? () {
+                        // TODO: 다음 페이지 이동
+                      }
+                    : null,
+                child: const Text('다음'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
