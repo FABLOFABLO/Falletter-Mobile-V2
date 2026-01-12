e3a5527b6479ee122f10e0cd1721ca7418349cdf
최승우 <choi0250655@gmail.com>
2026-01-12 18:58:32 +0900

    색상 상태관리 처리, 레터 사이즈 조절
    색상 상태관리 처리, 레터 사이즈 조절,
    
    title 쓴 이유 : 약관동의나 설정쪽에서 사용되기 때문입니다.

--- lib/core/components/app_bar/app_bar.dart
+++ lib/core/components/app_bar/app_bar.dart
@@ -1,73 +1,86 @@
 import 'package:falletter_mobile_v2/core/constants/color.dart';
 import 'package:falletter_mobile_v2/core/constants/text_style.dart';
+import 'package:falletter_mobile_v2/core/providers/theme/theme_state.dart';
+import 'package:falletter_mobile_v2/core/theme/app_theme_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:material_symbols_icons/symbols.dart';
 
-enum Action { brick, letter, order }
+enum Action { brickCount, letterCount, orderStep }
 
-class IndexAppBar extends StatelessWidget implements PreferredSizeWidget {
+class CustomAppBar extends ConsumerWidget implements PreferredSizeWidget {
   final bool icon;
   final String? title;
-  final Action action;
+  final Action? action;
   final int? count;
 
-  const IndexAppBar({
+  const CustomAppBar({
     super.key,
     required this.icon,
     this.title,
     this.count,
-    required this.action,
+    this.action,
   });
 
-  final String brick = 'assets/svg/brick/brick_blue.svg';
-  final String letter = 'assets/svg/letter/letter_blue.svg';
-
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsetsGeometry.symmetric(horizontal: 20),
-      child: AppBar(
-        centerTitle: true,
-        backgroundColor: FalletterColor.black,
-        leading: GestureDetector(
-          onTap: () => Navigator.of(context).pop(),
-          child: icon == true
-              ? Icon(
-                  Symbols.arrow_back_ios,
-                  size: 24,
-                  color: FalletterColor.white,
-                )
-              : null,
-        ),
-        title: Text(title ?? '', style: FalletterTextStyle.body1),
-        leadingWidth: 20,
-        actions: [
-          Row(
-            children: [
-              switch (action) {
-                Action.brick => Row(
-                  children: [
-                    SvgPicture.asset(brick, height: 38, width: 36),
-                    const SizedBox(width: 12),
-                    Text('$count개', style: FalletterTextStyle.body1),
-                  ],
-                ),
-                Action.letter => Row(
-                  children: [
-                    SvgPicture.asset(letter, height: 42, width: 40),
-                    const SizedBox(width: 12),
-                    Text('$count개', style: FalletterTextStyle.body1),
-                  ],
-                ),
-                Action.order => Text(
-                  '$count/5',
-                  style: FalletterTextStyle.body1,
-                ),
-              },
-            ],
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectedTheme = ref.watch(themeProvider);
+    final themeColors = appThemeColors[selectedTheme]!;
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsetsGeometry.symmetric(horizontal: 20),
+        child: AppBar(
+          centerTitle: true,
+          backgroundColor: FalletterColor.black,
+          leading: GestureDetector(
+            onTap: () => Navigator.of(context).pop(),
+            child: icon == true
+                ? Icon(
+                    Symbols.arrow_back_ios,
+                    size: 24,
+                    color: FalletterColor.white,
+                  )
+                : null,
           ),
-        ],
+          title: Text(title ?? '', style: FalletterTextStyle.body1),
+          leadingWidth: 20,
+          actions: [
+            Row(
+              children: [
+                switch (action) {
+                  Action.brickCount => Row(
+                    children: [
+                      SvgPicture.asset(
+                        themeColors.brickSvg,
+                        height: 38,
+                        width: 36,
+                      ),
+                      const SizedBox(width: 12),
+                      Text('$count개', style: FalletterTextStyle.body1),
+                    ],
+                  ),
+                  Action.letterCount => Row(
+                    children: [
+                      SvgPicture.asset(
+                        themeColors.letterSvg,
+                        height: 24,
+                        width: 35,
+                      ),
+                      const SizedBox(width: 12),
+                      Text('$count개', style: FalletterTextStyle.body1),
+                    ],
+                  ),
+                  Action.orderStep => Text(
+                    '$count/5',
+                    style: FalletterTextStyle.body1,
+                  ),
+                  null => const SizedBox.shrink(),
+                },
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
