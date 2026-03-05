5706dbaefa8f59a02b384f0714590c51fa020262
최승우 <choi0250655@gmail.com>
2026-03-05 22:08:23 +0900

    feat : 테마 선택 뷰 제작 (#20)
    * feat : 테마 선택 뷰 제작
    
    테마 선택 뷰 제작

--- lib/core/components/button/selectable_button.dart
+++ lib/core/components/button/selectable_button.dart
@@ -13,7 +13,6 @@ class SelectableButton extends ConsumerWidget {
   final Gradient? iconGradient;
   final bool isSelected;
   final VoidCallback onTap;
-
   const SelectableButton({
     super.key,
     required this.label,
@@ -35,11 +34,11 @@ class SelectableButton extends ConsumerWidget {
       child: Container(
         decoration: isSelected
             ? BoxDecoration(
-          gradient: themeColors.answerButton,
+          gradient: iconGradient ?? themeColors.answerButton,
           borderRadius: BorderRadius.circular(8),
         )
             : null,
-        padding: isSelected ? const EdgeInsets.all(2) : EdgeInsets.zero,
+        padding: isSelected ? const EdgeInsets.all(3) : EdgeInsets.zero,
         child: Container(
           decoration: BoxDecoration(
             color: FalletterColor.middleBlack,
