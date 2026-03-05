b870889da821dbc94e012cda8159ed9712594ff1
leejia0324 <hx.un0707@gmail.com>
2026-03-05 19:13:45 +0900

    feat: timer api service 코드 작성

---
+++ lib/core/providers/timer_api_service.dart
@@ -0,0 +1,58 @@
+import 'package:dio/dio.dart';
+import 'package:falletter_mobile_v2/core/network/api_endpoints.dart';
+import 'package:falletter_mobile_v2/models/timer_model.dart';
+
+class TimerApiService {
+  final Dio dio;
+
+  TimerApiService(this.dio);
+
+  Future<TimerModel> getRouletteTimer() async {
+    try {
+      final response = await dio.get(ApiEndpoints.rouletteTimer);
+
+      if (response.statusCode == 200) {
+        return TimerModel.fromJson(response.data);
+      }
+      throw Exception('Failed to get roulette timer');
+    } catch(e) {
+      print('Failed to get roulette timer: $e');
+      rethrow;
+    }
+  }
+
+  Future<void> startRouletteTimer() async {
+    try {
+      final response = await dio.post(ApiEndpoints.rouletteTimer);
+
+      if (response.statusCode == 201) return;
+      throw Exception('Failed to start roulette timer');
+    } catch(e) {
+      throw Exception('Failed to start roulette timer: $e');
+    }
+  }
+
+  Future<TimerModel> getBrickTimer() async {
+    try {
+      final response = await dio.get(ApiEndpoints.brickTimer);
+
+      if (response.statusCode == 200) {
+        return TimerModel.fromJson(response.data);
+      }
+      throw Exception('Failed to get brick timer');
+    } catch(e) {
+      throw Exception('Failed to get brick timer: $e');
+    }
+  }
+
+  Future<void> startBrickTimer() async {
+    try {
+      final response = await dio.post(ApiEndpoints.brickTimer);
+
+      if (response.statusCode == 201) return;
+      throw Exception('Failed to start brick timer');
+    } catch(e) {
+      throw Exception('Failed to start brick timer: $e');
+    }
+  }
+}
